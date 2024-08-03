60d77f176ecc5bc49119097378268e4c1fdecab0
hugogatti <hugo.gatti@hotmail.com>
2024-08-03 15:17:40 -0300

    Testes Melhorado

--- Testes Unitarios/Introdução/enderecos/endereco_test.go
+++ Testes Unitarios/Introdução/enderecos/endereco_test.go
@@ -1,16 +1,31 @@
+// TESTES UNITARIO COM DIVERSOS CENARIOS
 package enderecos
 
 import "testing"
 
-//TESTES UNITARIO BASICOS
+type cenarioDeTeste struct {
+	enderecoInserido string
+	retornoEsperado string
+}
 
 func TestTipoDeEndereco(t *testing.T) {
-	//FOMATO OBRIGADORIO o nome do arquivo tem que ter "_test" e o nome da func tem que ser neste formato: TestNomeDaSuaFunçao
-	enderecoParaTeste := "Rua Manuel Justiniano Quintão"
-	tipoDeEnderecoEsperado := "Avenida"
-	tipoDeEnderecoRecebido := TipoDeEndereco(enderecoParaTeste)
 
-	if tipoDeEnderecoRecebido != tipoDeEnderecoEsperado {
-		t.Errorf("O tipo recebido é diferente do esperado! Esperava %s e recebeu %s", tipoDeEnderecoEsperado, tipoDeEnderecoRecebido)
+	cenarioDeTeste := []cenarioDeTeste {
+		{ "Rua ABC", "Rua" },
+		{ "Avenida Paulista", "Avenida" },
+		{ "Rodovia dos Imigrantes", "Rodovia" },
+		{ "Praça do Cruzeiro", "Tipo Inválido" },
+		{ "Estrada dos Loucos", "Estrada" },
+		{ "RUA DOS BOBOS", "Rua" },
+		{ "AVENIDA REBOUÇAS", "Avenida" },
+		{ "", "Tipo Inválido" },
+	}
+
+	for _, cenario := range cenarioDeTeste {
+		tipoDeEnderecoRecebido := TipoDeEndereco(cenario.enderecoInserido)
+
+		if tipoDeEnderecoRecebido != cenario.retornoEsperado {
+			t.Errorf("O tipo recebido %s é diferente do esperado %s", tipoDeEnderecoRecebido, cenario.retornoEsperado)			
+		}
 	}
 }
